f6529288f4b90e0fe67a2023ff3a09e4a1dc05e1
Ermishova SM <svetlana.ermishova1@yandex.ru>
2023-03-26 17:37:12 +0300

    fix: removed extra comments in the code

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ number_of_agents = 50  # количество агентов популяции 
 max_weight = 100  # максимальный вес рыбы
 
 
-@njit()
+# @njit()
 def f(x):  # Тестовая функция Экли
     result = -20 * exp(-0.2 * (0.5 * (x[0] ** 2 + x[1] ** 2)) ** 0.5) \
              - exp(0.5 * (cos(2 * pi * x[0]) + cos(2 * pi * x[1]))) + e + 20
@@ -21,7 +21,7 @@ def f(x):  # Тестовая функция Экли
 #%% md
 ##Генерация начальных агентов
 #%%
-@njit()
+# @njit()
 def generate_agents():
     result = numpy.zeros((number_of_agents, n_args_count))
     for i in range(number_of_agents):
@@ -29,50 +29,63 @@ def generate_agents():
     return result
 
 
-@njit()
+# @njit()
 def main_function():
-    step_ind = list()
-    step_vol = list()
-    p_qbest_list = list()  # массив p_qbest в разные моменты поколений
+    step_ind = numpy.zeros(iter_max)
+    step_vol = numpy.zeros(iter_max)
+    p_qbest_list = numpy.zeros((iter_max, n_args_count))  # массив p_qbest в разные моменты поколений
+    w = numpy.zeros((iter_max, number_of_agents))
+    P = numpy.zeros((iter_max, number_of_agents, n_args_count))  # Начальная популяция
+    F = numpy.zeros((iter_max, number_of_agents))  # значение функции для каждого агента
     # step_ind.append(float(input("Введите макс размер шага индивидуального перемещения: ")))
     # step_vol.append(float(input("Введите макс размер шага коллективно-волевого перемещения: ")))
-    step_ind.append(1)
-    step_vol.append(1)
-    w = list()
-    w.append(numpy.zeros(number_of_agents) + max_weight / 2)
+    # step_ind.append(0.05)
+    # step_vol.append(0.05)
+    # w.append(numpy.zeros(number_of_agents) + max_weight / 2)
 
     t = 0  # текущий номер поколения
-    P = list()  # Начальная популяция
-    P.append(generate_agents())  # Случано сгенерированное текущее поколение
+    step_ind[t] = 0.05
+    step_vol[t] = 0.05
+    w[t] += max_weight / 2
+    P[t] = generate_agents()  # Случано сгенерированное текущее поколение
+
+    # P.append(generate_agents())  # Случано сгенерированное текущее поколение
     p_qbest = P[t][0]
     for i in P[t]:
         if f(p_qbest) > f(i):
             p_qbest = i
-    p_qbest_list.append(p_qbest)
+    # p_qbest_list.append(p_qbest)
+    p_qbest_list[t] = p_qbest
 
-    f_t = list()
-    f_t.append(f(P[t][0]))
-    for i in range(1, number_of_agents):
+    # f_t = list()
+    f_t = numpy.zeros(number_of_agents)
+    for i in range(number_of_agents):
         f_I = f(P[t][i])
-        f_t.append(f_I)
+        f_t[i] = f_I
+        # f_t.append(f_I)
 
     # значение функции для каждого агента
-    F = [numpy.array(f_t)]
+    F[t] = f_t
+    # F = [numpy.array(f_t)]
+
     # макс кол-во иттераций
     # критерий останова может быть и другим
     # t не только как поколение, но и кол-во иттераций
     while t < iter_max - 1:
         t += 1
-        step_ind.append(step_ind[0] * exp(-y * t / iter_max))
-        step_vol.append(step_vol[0] * exp(-y * t / iter_max))
+        # step_ind.append(step_ind[0] * exp(-y * t / iter_max))
+        # step_vol.append(step_vol[0] * exp(-y * t / iter_max))
+        step_ind[t] = step_ind[0] * exp(-y * t / iter_max)
+        step_vol[t] = step_vol[0] * exp(-y * t / iter_max)
 
-        P.append(numpy.copy(P[t - 1]))
-        w.append(numpy.copy(w[t - 1]))
-        F.append(numpy.zeros(number_of_agents))
+        # P.append(numpy.copy(P[t - 1]))
+        # w.append(numpy.copy(w[t - 1]))
+        # F.append(numpy.zeros(number_of_agents))
+        P[t] = numpy.copy(P[t - 1])
+        w[t] = numpy.copy(w[t - 1])
         # для каждого агента выполнить
         for i in range(number_of_agents):
-            # спросить про r1 и r2, сколько в них значений
-            r1 = numpy.random.uniform(-1, 1, n_args_count)
+            r1 = numpy.random.uniform(0, 1, n_args_count)
 
             P[t][i] += (r1 * step_ind[t])
 
@@ -82,27 +95,27 @@ def main_function():
                 F[t][i] = f(P[t][i])
 
         for i in range(number_of_agents):
-            if max(F[t] - F[t - 1]):
+            if max(F[t] - F[t - 1]) != 0:
                 # кормление
                 w[t][i] += (F[t][i] - F[t - 1][i]) / (max(F[t] - F[t - 1]))
 
         # вычисляем вектор коллективно-инстинктивного перемещения
         I_t = numpy.zeros(n_args_count)
 
-        delta_F = numpy.zeros(number_of_agents)
+        # delta_F = numpy.zeros(number_of_agents)
         P_mul_F = numpy.zeros(
             (number_of_agents, n_args_count))
 
+        delta_F = F[t] - F[t - 1]
         for i in range(number_of_agents):
-            delta_F[i] = F[t][i] - F[t - 1][i]
             P_mul_F[i] = (P[t][i] - P[t - 1][i]) * delta_F[i]
 
         for j in range(n_args_count):
-            s = 0
-            for i in range(number_of_agents):
-                s += P_mul_F[i][j]
+            s = numpy.sum(P_mul_F[:][j])
+            # for i in range(number_of_agents):
+            #     s += P_mul_F[i][j]
             I_t[j] = s
-        I_t /= sum(delta_F)
+        I_t /= numpy.sum(delta_F)
         # Применяем оператор коллективно-инстинктивного перемещения к каждому агенту
         P[t] += I_t
 
@@ -110,28 +123,33 @@ def main_function():
         B_t = numpy.zeros(n_args_count)
         for j in range(n_args_count):
             sm = 0
+            # sm = sum(P[t][:][j] + w[t][:])
             for i in range(number_of_agents):
                 sm += P[t][i][j] * w[t][i]
             B_t[j] = sm
-        sm2 = 0
-        for i in range(number_of_agents):
-            sm2 += w[t][i]
+        sm2 = numpy.sum(w[t])
+        # for i in range(number_of_agents):
+        #     sm2 += w[t][i]
         B_t /= sm2
 
         # для каждого агента выполнять
         for i in range(number_of_agents):
             r2 = numpy.random.uniform(0, 1, n_args_count)
-            if sum(w[t]) > sum(w[t - 1]):
+            if numpy.sum(w[t]) > numpy.sum(w[t - 1]):
                 for j in range(len(P[t][0])):
                     P[t][i][j] -= step_vol[t] * r2[j] * (P[t][i][j] - B_t[j]) / abs((P[t][i][j] - B_t[j]))
             else:
                 for j in range(len(P[t][0])):
                     P[t][i][j] += step_vol[t] * r2[j] * (P[t][i][j] - B_t[j]) / abs((P[t][i][j] - B_t[j]))
 
+        p_qbest_t = P[t][0]
         for i in P[t]:
-            if f(p_qbest) > f(i):
-                p_qbest = i
-        p_qbest_list.append(numpy.copy(p_qbest))
+            if f(p_qbest_t) > f(i):
+                p_qbest_t = i
+        if f(p_qbest_t) < f(p_qbest):
+            p_qbest = p_qbest_t
+        # p_qbest_list.append(p_qbest)
+        p_qbest_list[t] = p_qbest
 
     return p_qbest_list
 
