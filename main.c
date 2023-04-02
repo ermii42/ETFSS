9fdc8fe21f3f79820064d9c9189c308cb388c7c4
Ermishova SM <svetlana.ermishova1@yandex.ru>
2023-04-02 17:54:39 +0300

    feat: added not working full code on C

--- main.c
+++ main.c
@@ -9,79 +9,86 @@ float random_uniform(float min, float max) {
     return min + (max - min) / RAND_MAX * rand();
 }
 
-float *add_arrays(float arr[], float plus[], int n_args_count) {
+float *add_arrays(const float arr[], const float plus[], int n_args_count) {
     float *result = (float *) malloc(n_args_count * sizeof(float));
-    result = arr;
     for (int i = 0; i < n_args_count; i++) {
-        result[i] += plus[i];
+        result[i] = arr[i] + plus[i];
     }
     return result;
 }
 
-float *sub_arrays(float arr[], float minus[], int n_args_count) {
+float *sub_arrays(const float arr[], const float minus[], int n_args_count) {
     float *result = (float *) malloc(n_args_count * sizeof(float));
-    result = arr;
     for (int i = 0; i < n_args_count; i++) {
-        result[i] -= minus[i];
+        result[i] = arr[i] - minus[i];
     }
     return result;
 }
 
-float *mul_arrays(float arr[], float mul[], int n_args_count) {
+float *mul_arrays(const float arr[], const float mul[], int n_args_count) {
     float *result = (float *) malloc(n_args_count * sizeof(float));
-    result = arr;
     for (int i = 0; i < n_args_count; i++) {
-        result[i] *= mul[i];
+        result[i] = arr[i] * mul[i];
     }
     return result;
 }
 
-float *div_arrays(float arr[], float div[], int n_args_count) {
+float *div_arrays(const float arr[], const float div[], int n_args_count) {
     float *result = (float *) malloc(n_args_count * sizeof(float));
-    result = arr;
     for (int i = 0; i < n_args_count; i++) {
-        result[i] /= div[i];
+        result[i] = arr[i] / div[i];
     }
     return result;
 }
 
-float *add_array_number(float arr[], float number, int n_args_count) {
+float *add_array_number(const float arr[], float number, int n_args_count) {
     float *result = (float *) malloc(n_args_count * sizeof(float));
-    result = arr;
     for (int i = 0; i < n_args_count; i++) {
-        result[i] += number;
+        result[i] = arr[i] + number;
     }
     return result;
 }
 
-float *sub_array_number(float arr[], float number, int n_args_count) {
+float *sub_array_number(const float arr[], float number, int n_args_count) {
     float *result = (float *) malloc(n_args_count * sizeof(float));
-    result = arr;
     for (int i = 0; i < n_args_count; i++) {
-        result[i] -= number;
+        result[i] = arr[i] - number;
     }
     return result;
 }
 
-float *mul_array_number(float arr[], float number, int n_args_count) {
+float *mul_array_number(const float arr[], float number, int n_args_count) {
     float *result = (float *) malloc(n_args_count * sizeof(float));
-    result = arr;
     for (int i = 0; i < n_args_count; i++) {
-        result[i] *= number;
+        result[i] = arr[i] * number;
     }
     return result;
 }
 
-float *div_array_number(float arr[], float number, int n_args_count) {
+float *div_array_number(const float arr[], float number, int n_args_count) {
     float *result = (float *) malloc(n_args_count * sizeof(float));
-    result = arr;
     for (int i = 0; i < n_args_count; i++) {
-        result[i] /= number;
+        result[i] = arr[i] / number;
     }
     return result;
 }
 
-float max(float arr[], int n) {
+void copy_1d(float *arr1, const float arr2[], int n) {
+//    float *result = (float *) malloc(n * sizeof(float));
+    for (int i = 0; i < n; i++) {
+        arr1[i] = arr2[i];
+    }
+}
+
+void copy_2d(float **arr1, const float *arr2[], int n, int m) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            arr1[i][j] = arr2[i][j];
+        }
+    }
+}
+
+float max(const float arr[], int n) {
     float result = arr[0];
     for (int i = 1; i < n; i++) {
         if (result < arr[i]) {
@@ -91,10 +98,7 @@ float max(float arr[], int n) {
     return result;
 }
 
-//void main_function(float* (*f)(float *, float), float** (*generate_agents)(float *, float), int n_args_count, int iter_max, int number_of_agents, float max_weight){
-//    float step_ind[iter_max]
-//}
-float f1(float *x, int n_args_count) { // Тестовая функция Растригина
+float f1(const float *x, int n_args_count) { // Тестовая функция Растригина
     float A = 10;
     float result = 0;
     for (int i = 0; i < n_args_count; i++) {
@@ -112,14 +116,89 @@ void generate_agents1(float **arr, int number_of_agents, int n_args_count) {
         }
     }
 }
-//void test(){
-//    float *step_ind = (float *) malloc(iter_max);
-//}
+
+void output_array2d(float **arr, int n, int m) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            printf("%f ", arr[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void output_array1d(float *arr, int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%f ", arr[i]);
+    }
+    printf("\n");
+}
+
+float sum_2d(float **arr, int n, int j) {
+    float result = 0;
+    for (int i = 0; i < n; i++) {
+        result += arr[i][j];
+    }
+    return result;
+}
+
+float sum_1d(float *arr, int n) {
+    float result = 0;
+    for (int i = 0; i < n; i++) {
+        result += arr[i];
+    }
+    return result;
+}
+
+void test() {
+    int n = 10;
+    int m = 3;
+    float **arr1 = (float **) malloc(n * sizeof(float *));
+    float **arr2 = (float **) malloc(n * sizeof(float *));
+    for (int i = 0; i < n; i++) {
+        arr1[i] = (float *) malloc(m * sizeof(float));
+        arr2[i] = (float *) malloc(m * sizeof(float));
+    }
+    for (int i = 0; i < n; i++) {
+        for(int j=0; j<m; j++){
+            arr1[i][j] = i + j;
+            arr2[i][j] = 2;
+        }
+    }
+    output_array2d(arr1, n, m);
+    printf("\n");
+    output_array2d(arr2, n, m);
+    copy_2d(arr1, arr2, n, m);
+    arr1[0][0] = -1;
+    printf("\n");
+    output_array2d(arr1, n, m);
+    printf("\n");
+    output_array2d(arr2, n, m);
+//    output_array1d(arr1, n);
+//    output_array1d(arr2, n);
+//    printf("\n");
+//    copy_1d(arr1, arr2, n);
+////    arr1 = arr2;
+//    arr1[0] = 1;
+//    output_array1d(arr1, n);
+//    output_array1d(arr2, n);
+//    output_array1d(mul_arrays(arr1, arr2, n), n);
+//    output_array1d(div_arrays(arr1, arr2, n), n);
+//    output_array1d(add_arrays(arr1, arr2, n), n);
+//    output_array1d(sub_arrays(arr1, arr2, n), n);
+//    printf("\n");
+//
+//    output_array1d(mul_array_number(arr1, m, n), n);
+//    output_array1d(div_array_number(arr1, m, n), n);
+//    output_array1d(add_array_number(arr1, m, n), n);
+//    output_array1d(sub_array_number(arr1, m, n), n);
+//    printf("\n");
+//    printf("%f", sum_1d(arr1, n));
+}
 
 void main_function(float (*f)(float *, int), void (*generate_agents)(float **, int, int), int n_args_count,
                    int iter_max, int number_of_agents, float max_weight) {
 
-
+    srand(time(NULL));
     int y = 5;
     float *step_ind = (float *) malloc(iter_max);
     float *step_vol = (float *) malloc(iter_max);
@@ -145,18 +224,19 @@ void main_function(float (*f)(float *, int), void (*generate_agents)(float **, i
     int t = 0;
     step_ind[t] = 0.05f;
     step_vol[t] = 0.05f;
-    // w[t] += 50
+    // w[t] = 50
     for (int i = 0; i < number_of_agents; i++) {
         w[t][i] = max_weight / 2;
     }
     // P[t] = generate agents
     generate_agents(P[t], number_of_agents, n_args_count);
     float *p_qbest = (float *) malloc(n_args_count * sizeof(float));
-    p_qbest = P[t][0];
+    copy_1d(p_qbest, P[t][0], n_args_count);
     for (int i = 1; i < number_of_agents; i++) {
 
         if (f(p_qbest, n_args_count) > f(P[t][i], n_args_count)) {
-            p_qbest = P[t][i];
+//            p_qbest = P[t][i];
+            copy_1d(p_qbest, P[t][i], n_args_count);
         }
     }
     p_qbest_list[t] = p_qbest;
@@ -173,14 +253,18 @@ void main_function(float (*f)(float *, int), void (*generate_agents)(float **, i
     for (int i = 0; i < number_of_agents; i++) {
         P_mul_F[i] = (float *) malloc(n_args_count * sizeof(float));
     }
-    float delta_F;
+    float *delta_F;
+    float *B_t = (float *) malloc(n_args_count * sizeof(float));
+    float sm = 0;  // сумма
+    float sm2 = 0;
+    float *p_qbest_t = (float *) malloc(n_args_count * sizeof(float));
     while (t < iter_max - 1) {
         t++;
         step_ind[t] = step_ind[0] * exp(-y * t / iter_max);
         step_vol[t] = step_vol[0] * exp(-y * t / iter_max);
 
-        P[t] = P[t - 1];
-        P[t] = P[t - 1];
+        copy_2d(P[t], (const float **) P[t - 1], number_of_agents, n_args_count);
+        copy_1d(w[t], w[t - 1], number_of_agents);
         // для каждого агента выполнить
         for (int i = 0; i < number_of_agents; i++) {
             for (int j = 0; j < number_of_agents; j++) {
@@ -190,53 +274,81 @@ void main_function(float (*f)(float *, int), void (*generate_agents)(float **, i
 
             F[t][i] = f(P[t][i], n_args_count); // значение f для P_it
             if (F[t][i] >= F[t - 1][i]) {
-                P[t][i] = P[t - 1][i];
+                copy_1d(P[t][i], P[t - 1][i], n_args_count);
+//                P[t][i] = P[t - 1][i];
                 F[t][i] = f(P[t][i], n_args_count);
             }
         }
-//        for (int i = 0; i < number_of_agents; i++) {
-//            if (max(sub_arrays(F[t], F[t - 1], number_of_agents), number_of_agents) != 0) {
-//                // кормление
-//                w[t][i] =
-//                        (F[t][i] - F[t - 1][i]) / (max(sub_arrays(F[t], F[t - 1], number_of_agents), number_of_agents));
-//            }
-//        }
+        for (int i = 0; i < number_of_agents; i++) {
+            if (max(sub_arrays(F[t], F[t - 1], number_of_agents), number_of_agents) != 0) {
+                // кормление
+                w[t][i] +=
+                        (F[t][i] - F[t - 1][i]) / (max(sub_arrays(F[t], F[t - 1], number_of_agents), number_of_agents));
+            }
+        }
         // вычисляем вектор коллективно-инстинктивного перемещения
-//        delta_F = F[t] - F[t - 1];
-//        for(int i=0; i<number_of_agents; i++){
-//            P_mul_F[i] = mul_arrays()
-//        }
+        delta_F = sub_arrays(F[t], F[t - 1], number_of_agents);
+        for (int i = 0; i < number_of_agents; i++) {
+            P_mul_F[i] = mul_array_number(sub_arrays(P[t][i], P[t - 1][i], n_args_count), delta_F[i], n_args_count);
+        }
+        for (int j = 0; j < n_args_count; j++) {
+            I_t[j] = sum_2d(P_mul_F, number_of_agents, j);
+        }
+        I_t = div_array_number(I_t, sum_1d(delta_F, number_of_agents), n_args_count);
+        // Применяем оператор коллективно-инстинктивного перемещения к каждому агенту
+        for (int i = 0; i < number_of_agents; i++) {
+            P[t][i] = add_arrays(P[t][i], I_t, n_args_count);
+        }
+
+        // Вычисляем Бариоцентр
+        for (int j = 0; j < n_args_count; j++) {
+            sm = 0;
+            for (int i = 0; i < number_of_agents; i++) {
+                sm += P[t][i][j] * w[t][i];
+            }
+            B_t[j] = sm;
+        }
+        sm2 = sum_1d(w[t], number_of_agents);
+        B_t = div_array_number(B_t, sm2, n_args_count);
+
+        // для каждого агента выполнять
+        for (int i = 0; i < number_of_agents; i++) {
+            for (int j = 0; j < number_of_agents; j++) {
+                r2[j] = random_uniform(0, 1) * step_ind[t];
+            }
+            if (sum_1d(w[t], number_of_agents) > sum_1d(w[t - 1], number_of_agents)) {
+                for (int j = 0; j < n_args_count; j++) {
+                    P[t][i][j] -= step_vol[t] * r2[j] * (P[t][i][j] - B_t[j]) / fabsf((P[t][i][j] - B_t[j]));
+                }
+            } else {
+                for (int j = 0; j < n_args_count; j++) {
+                    P[t][i][j] += step_vol[t] * r2[j] * (P[t][i][j] - B_t[j]) / fabsf((P[t][i][j] - B_t[j]));
+                }
+            }
+        }
+        copy_1d(p_qbest_t, P[t][0], n_args_count);
+        for (int i = 0; i < number_of_agents; i++) {
+            if (f(p_qbest_t, n_args_count) > f(P[t][i], n_args_count)) {
+                copy_1d(p_qbest_t, P[t][i], n_args_count);
+            }
+        }
+        if (f(p_qbest_t, n_args_count) < f(p_qbest, n_args_count)) {
+            copy_1d(p_qbest, p_qbest_t, n_args_count);
+        }
+        copy_1d(p_qbest_list[t], p_qbest, n_args_count);
     }
-//    for(int i=0; i<number_of_agents; i++){
-//        for (int j = 0; j < n_args_count; j++) {
-//            printf("%f ", P[t][i][j]);
-//        }
-//        printf("\n");
-//    }
-//    printf("\n");
-//    printf("%f %f", p_qbest[0], p_qbest[1]);
-//    printf("\n");
-//
-//
-//    for(int i=0; i<number_of_agents; i++){
-//        printf("%f ", F[t][i]);
-//    }
-//    printf("\n");
-//    for(int i=0; i<number_of_agents; i++){
-//        printf("%f ", w[t][i]);
-//    }
-//    printf("\n");
+    output_array1d(p_qbest, n_args_count);
+    printf("%f", f(p_qbest, n_args_count));
 }
 
 int main() {
-    srand(time(NULL));
+
     printf("Hello, World!\n");
-    int number_of_agents_ = 2;
+    int number_of_agents_ = 50;
     int n_args_count_ = 2;
-    float *arr = (float *) malloc(2);
-    arr[0] = -3.300257;
-    arr[1] = 4.033404;
-    main_function(f1, generate_agents1, n_args_count_, 2, number_of_agents_, 100);
-//    printf("%f", f1(arr, 2));
+    int iter = 500;
+    main_function(f1, generate_agents1, n_args_count_, iter, number_of_agents_, 100);
+
+//    test();
     return 0;
 }
--- main.py
+++ main.py
@@ -1,63 +1,37 @@
-import time
-from math import cos, pi, exp, e
+from math import cos, pi, exp
 import numpy
-from numba import njit
-import matplotlib.pyplot as plt
 
-#%%
-y = 5
-n_args_count = 2  # количество x-ов, подаваемых в тестовую функцию
-iter_max = 500  # больше 500 не нужно
-number_of_agents = 50  # количество агентов популяции (20-40-50 должно быть)
-max_weight = 100  # максимальный вес рыбы
+def f1(x):  # Тестовая функция Растригина
+    A = 10
+    result = 0
+    for i in x:
+        result += i * i - A * cos(2 * pi * i)
+    result += A * len(x)
 
-
-# @njit()
-def f(x):  # Тестовая функция Экли
-    result = -20 * exp(-0.2 * (0.5 * (x[0] ** 2 + x[1] ** 2)) ** 0.5) \
-             - exp(0.5 * (cos(2 * pi * x[0]) + cos(2 * pi * x[1]))) + e + 20
     return result
 
-#%% md
-##Генерация начальных агентов
-#%%
-# @njit()
-def generate_agents():
-    result = numpy.zeros((number_of_agents, n_args_count))
-    for i in range(number_of_agents):
-        result[i] = numpy.random.uniform(-5, 5, n_args_count)
-    return result
+def main_function(f, generate_agents, n_args_count, iter_max, number_of_agents, max_weight):
+    y = 5
 
-
-# @njit()
-def main_function():
     step_ind = numpy.zeros(iter_max)
     step_vol = numpy.zeros(iter_max)
     p_qbest_list = numpy.zeros((iter_max, n_args_count))  # массив p_qbest в разные моменты поколений
     w = numpy.zeros((iter_max, number_of_agents))
     P = numpy.zeros((iter_max, number_of_agents, n_args_count))  # Начальная популяция
     F = numpy.zeros((iter_max, number_of_agents))  # значение функции для каждого агента
-    # step_ind.append(float(input("Введите макс размер шага индивидуального перемещения: ")))
-    # step_vol.append(float(input("Введите макс размер шага коллективно-волевого перемещения: ")))
-    # step_ind.append(0.05)
-    # step_vol.append(0.05)
-    # w.append(numpy.zeros(number_of_agents) + max_weight / 2)
 
     t = 0  # текущий номер поколения
     step_ind[t] = 0.05
     step_vol[t] = 0.05
     w[t] += max_weight / 2
-    P[t] = generate_agents()  # Случано сгенерированное текущее поколение
+    P[t] = generate_agents(number_of_agents, n_args_count)  # Случано сгенерированное текущее поколение
 
-    # P.append(generate_agents())  # Случано сгенерированное текущее поколение
     p_qbest = P[t][0]
     for i in P[t]:
         if f(p_qbest) > f(i):
             p_qbest = i
-    # p_qbest_list.append(p_qbest)
     p_qbest_list[t] = p_qbest
 
-    # f_t = list()
     f_t = numpy.zeros(number_of_agents)
     for i in range(number_of_agents):
         f_I = f(P[t][i])
@@ -66,21 +40,15 @@ def main_function():
 
     # значение функции для каждого агента
     F[t] = f_t
-    # F = [numpy.array(f_t)]
 
     # макс кол-во иттераций
     # критерий останова может быть и другим
     # t не только как поколение, но и кол-во иттераций
     while t < iter_max - 1:
         t += 1
-        # step_ind.append(step_ind[0] * exp(-y * t / iter_max))
-        # step_vol.append(step_vol[0] * exp(-y * t / iter_max))
         step_ind[t] = step_ind[0] * exp(-y * t / iter_max)
         step_vol[t] = step_vol[0] * exp(-y * t / iter_max)
 
-        # P.append(numpy.copy(P[t - 1]))
-        # w.append(numpy.copy(w[t - 1]))
-        # F.append(numpy.zeros(number_of_agents))
         P[t] = numpy.copy(P[t - 1])
         w[t] = numpy.copy(w[t - 1])
         # для каждого агента выполнить
@@ -102,7 +70,6 @@ def main_function():
         # вычисляем вектор коллективно-инстинктивного перемещения
         I_t = numpy.zeros(n_args_count)
 
-        # delta_F = numpy.zeros(number_of_agents)
         P_mul_F = numpy.zeros(
             (number_of_agents, n_args_count))
 
@@ -112,8 +79,6 @@ def main_function():
 
         for j in range(n_args_count):
             s = numpy.sum(P_mul_F[:][j])
-            # for i in range(number_of_agents):
-            #     s += P_mul_F[i][j]
             I_t[j] = s
         I_t /= numpy.sum(delta_F)
         # Применяем оператор коллективно-инстинктивного перемещения к каждому агенту
@@ -123,13 +88,10 @@ def main_function():
         B_t = numpy.zeros(n_args_count)
         for j in range(n_args_count):
             sm = 0
-            # sm = sum(P[t][:][j] + w[t][:])
             for i in range(number_of_agents):
                 sm += P[t][i][j] * w[t][i]
             B_t[j] = sm
         sm2 = numpy.sum(w[t])
-        # for i in range(number_of_agents):
-        #     sm2 += w[t][i]
         B_t /= sm2
 
         # для каждого агента выполнять
@@ -148,24 +110,11 @@ def main_function():
                 p_qbest_t = i
         if f(p_qbest_t) < f(p_qbest):
             p_qbest = p_qbest_t
-        # p_qbest_list.append(p_qbest)
         p_qbest_list[t] = p_qbest
 
     return p_qbest_list
 
+print(f1([0,0]))
+#%%
 
-start = time.time()
-best_lst = main_function()
-best = best_lst[-1]
-end = time.time() - start
-print(*[f(i) for i in best_lst], sep='\n')
-print(f'Coordinates of p_qbest:\t\t\t{best}')
-print(f'The function value of pqbest:\t{f(best)}')
-print(f'Algorithm running time:\t\t\t{end} s')
-
-# f_val = [f(i) for i in best_lst]
-# plt.plot(numpy.arange(iter_max), f_val)
-# plt.xlabel("Номер поколения")
-# plt.ylabel("Лучшее значение на данный момент")
-# plt.title("Глобально лучшее решение для всех частиц")
-# plt.show()
+# print(f1([2.293351, -0.665176]))
