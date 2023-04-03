e7f18b4e06b88ceabbe7681ad4f38804c0d9e37b
Ermishova SM <svetlana.ermishova1@yandex.ru>
2023-04-04 00:45:45 +0300

    fix: working kode on C!

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 
 #define PI 3.1415926535897931159979635
 
-float random_uniform(float min, float max) {
+float random_uniform2(float min, float max) {
     return min + (max - min) / RAND_MAX * rand();
 }
 
@@ -112,7 +112,7 @@ float f1(const float *x, int n_args_count) { // Тестовая функция 
 void generate_agents1(float **arr, int number_of_agents, int n_args_count) {
     for (int i = 0; i < number_of_agents; i++) {
         for (int j = 0; j < n_args_count; j++) {
-            arr[i][j] = random_uniform(-5.12f, 5.12f);
+            arr[i][j] = random_uniform2(-5.12f, 5.12f);
         }
     }
 }
@@ -133,7 +133,7 @@ void output_array1d(float *arr, int n) {
     printf("\n");
 }
 
-float sum_2d(float **arr, int n, int j) {
+float sum_2d(const float **arr, int n, int j) {
     float result = 0;
     for (int i = 0; i < n; i++) {
         result += arr[i][j];
@@ -141,7 +141,7 @@ float sum_2d(float **arr, int n, int j) {
     return result;
 }
 
-float sum_1d(float *arr, int n) {
+float sum_1d(const float *arr, int n) {
     float result = 0;
     for (int i = 0; i < n; i++) {
         result += arr[i];
@@ -150,7 +150,7 @@ float sum_1d(float *arr, int n) {
 }
 
 void test() {
-    int n = 10;
+    int n = 500;
     int m = 3;
     float **arr1 = (float **) malloc(n * sizeof(float *));
     float **arr2 = (float **) malloc(n * sizeof(float *));
@@ -160,19 +160,23 @@ void test() {
     }
     for (int i = 0; i < n; i++) {
         for(int j=0; j<m; j++){
-            arr1[i][j] = i + j;
+            arr1[i][j] = 1;
             arr2[i][j] = 2;
         }
     }
+    for(int i=0; i<500; i++){
+        arr1[i] = add_arrays(arr1[i], arr2[i], m);
+    }
     output_array2d(arr1, n, m);
-    printf("\n");
-    output_array2d(arr2, n, m);
-    copy_2d(arr1, arr2, n, m);
-    arr1[0][0] = -1;
-    printf("\n");
-    output_array2d(arr1, n, m);
-    printf("\n");
-    output_array2d(arr2, n, m);
+//    output_array2d(arr1, n, m);
+//    printf("\n");
+//    output_array2d(arr2, n, m);
+//    copy_2d(arr1, (const float **) arr2, n, m);
+//    arr1[0][0] = -1;
+//    printf("\n");
+//    output_array2d(arr1, n, m);
+//    printf("\n");
+//    output_array2d(arr2, n, m);
 //    output_array1d(arr1, n);
 //    output_array1d(arr2, n);
 //    printf("\n");
@@ -200,8 +204,8 @@ void main_function(float (*f)(float *, int), void (*generate_agents)(float **, i
 
     srand(time(NULL));
     int y = 5;
-    float *step_ind = (float *) malloc(iter_max);
-    float *step_vol = (float *) malloc(iter_max);
+    float *step_ind = (float *) malloc(iter_max* sizeof(float ));
+    float *step_vol = (float *) malloc(iter_max* sizeof(float ));
     float **p_qbest_list = (float **) malloc(iter_max * sizeof(float *));
     for (int i = 0; i < iter_max; i++) {
         p_qbest_list[i] = (float *) malloc(n_args_count * sizeof(float));
@@ -240,6 +244,8 @@ void main_function(float (*f)(float *, int), void (*generate_agents)(float **, i
         }
     }
     p_qbest_list[t] = p_qbest;
+
+    // значение функции для каждого агента
     for (int i = 0; i < number_of_agents; i++) {
         F[t][i] = f1(P[t][i], n_args_count);
     }
@@ -259,16 +265,17 @@ void main_function(float (*f)(float *, int), void (*generate_agents)(float **, i
     float sm2 = 0;
     float *p_qbest_t = (float *) malloc(n_args_count * sizeof(float));
     while (t < iter_max - 1) {
-        t++;
+        t+=1;
         step_ind[t] = step_ind[0] * exp(-y * t / iter_max);
         step_vol[t] = step_vol[0] * exp(-y * t / iter_max);
+//        printf("%i\n", t);
 
         copy_2d(P[t], (const float **) P[t - 1], number_of_agents, n_args_count);
         copy_1d(w[t], w[t - 1], number_of_agents);
         // для каждого агента выполнить
         for (int i = 0; i < number_of_agents; i++) {
-            for (int j = 0; j < number_of_agents; j++) {
-                r1[j] = random_uniform(0, 1) * step_ind[t];
+            for (int j = 0; j < n_args_count; j++) {
+                r1[j] = random_uniform2(0, 1) * step_ind[t];
             }
             P[t][i] = add_arrays(P[t][i], r1, n_args_count);
 
@@ -292,7 +299,7 @@ void main_function(float (*f)(float *, int), void (*generate_agents)(float **, i
             P_mul_F[i] = mul_array_number(sub_arrays(P[t][i], P[t - 1][i], n_args_count), delta_F[i], n_args_count);
         }
         for (int j = 0; j < n_args_count; j++) {
-            I_t[j] = sum_2d(P_mul_F, number_of_agents, j);
+            I_t[j] = sum_2d((const float **) P_mul_F, number_of_agents, j);
         }
         I_t = div_array_number(I_t, sum_1d(delta_F, number_of_agents), n_args_count);
         // Применяем оператор коллективно-инстинктивного перемещения к каждому агенту
@@ -314,7 +321,7 @@ void main_function(float (*f)(float *, int), void (*generate_agents)(float **, i
         // для каждого агента выполнять
         for (int i = 0; i < number_of_agents; i++) {
             for (int j = 0; j < number_of_agents; j++) {
-                r2[j] = random_uniform(0, 1) * step_ind[t];
+                r2[j] = random_uniform2(0, 1) * step_ind[t];
             }
             if (sum_1d(w[t], number_of_agents) > sum_1d(w[t - 1], number_of_agents)) {
                 for (int j = 0; j < n_args_count; j++) {
@@ -347,7 +354,7 @@ int main() {
     int number_of_agents_ = 50;
     int n_args_count_ = 2;
     int iter = 500;
-    main_function(f1, generate_agents1, n_args_count_, iter, number_of_agents_, 100);
+    main_function((float (*)(float *, int)) f1, generate_agents1, n_args_count_, iter, number_of_agents_, 100);
 
 //    test();
     return 0;
